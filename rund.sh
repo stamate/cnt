06b8f54990dc01275c0fd0fac3c00aa1f0688c8b
cosmin <cosmin@dcs.bbk.ac.uk>
2022-09-30 09:41:20 +0100

    Removed naming from running scripts

--- rund.sh
+++ rund.sh
@@ -1,1 +1,1 @@
-docker run -d -v $(pwd):/lab --net=host --name $(pwd | xargs basename) nblab /bin/bash -c "(ls /lab/requirements.txt >> /dev/null 2>&1 && pip install -r /lab/requirements.txt) || echo 'No requirements.txt'; nohup jupyter notebook > /var/log/jupyter.log & >> disown"
+docker run -d -v $(pwd):/lab --net=host nblab /bin/bash -c "(ls /lab/requirements.txt >> /dev/null 2>&1 && pip install -r /lab/requirements.txt) || echo 'No requirements.txt'; nohup jupyter notebook > /var/log/jupyter.log & >> disown"
